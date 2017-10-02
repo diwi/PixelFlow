086843aa229b7e349c53bcd122ce743382f53b88
Thomas Diewald <diewald31415@gmail.com>
2017-10-02 18:39:44 +0200

    .

--- src/com/thomasdiewald/pixelflow/glsl/FlowFieldParticles/particles_display_quads.glsl
+++ src/com/thomasdiewald/pixelflow/glsl/FlowFieldParticles/particles_display_quads.glsl
@@ -14,12 +14,12 @@
 
 
 #define SHADER_VERT 0
-#define SHADER_FRAG_COLLISION 0
-#define SHADER_FRAG_DISPLAY 0
+#define SHADER_FRAG 0
 
 uniform float     point_size;
 uniform ivec2     wh_position;
-uniform  vec2     wh_viewport_rcp;
+uniform float     shader_collision_mult = 1.0;
+uniform sampler2D tex_collision;
 uniform sampler2D tex_position;
 uniform sampler2D tex_sprite;
 uniform vec4      col_A = vec4(1, 1, 1, 1.0);
@@ -28,6 +28,7 @@ uniform vec4      col_B = vec4(0, 0, 0, 0.0);
 #if SHADER_VERT
 
 out vec2 texcoord;
+out float pressure;
 
 void main(){
 
@@ -53,33 +54,28 @@ void main(){
   vec4 particle = texelFetch(tex_position, ivec2(col, row), 0);
   vec2 pos = particle.xy * 2.0 - 1.0;
   
+  float vel = length(pos - particle.zw) * 2000;
+  pressure = texture(tex_collision, pos).r + vel;
+  
   gl_Position  = vec4(vtx + pos, 0, 1); // ndc: [-1, +1]
 }
 
 #endif // #if SHADER_VERT
 
 
-#if SHADER_FRAG_COLLISION
 
-in vec2 texcoord;  
-out float out_frag;
+#if SHADER_FRAG
 
-void main(){
-  out_frag = max(0, 1.0 - length(texcoord * 2.0 - 1.0));
-}
-
-#endif // #if SHADER_FRAG
-
-
-
-#if SHADER_FRAG_DISPLAY
-
-in vec2 texcoord;  
+in vec2 texcoord;
+in float pressure;
 out vec4 out_frag;
 
 void main(){
   float falloff = texture(tex_sprite, texcoord).a;
   out_frag = mix(col_A, col_B, 1.0 - falloff);
+  float pf = 1.0 + pressure * shader_collision_mult;
+  out_frag.xyzw *= pf;
+  out_frag = clamp(out_frag, 0.0, 1.0);
 }
 
 #endif // #if SHADER_FRAG
