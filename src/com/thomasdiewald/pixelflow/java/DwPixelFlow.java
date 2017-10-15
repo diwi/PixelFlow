a7cb4f7da75f7a903f585d1c66b9df5d4cffd6eb
Thomas Diewald <diewald31415@gmail.com>
2017-10-15 04:46:33 +0200

    Release v1.15 (55)

--- src/com/thomasdiewald/pixelflow/java/DwPixelFlow.java
+++ src/com/thomasdiewald/pixelflow/java/DwPixelFlow.java
@@ -38,7 +38,7 @@ public class DwPixelFlow{
                                      
   static public class PixelFlowInfo{
     
-    static public final String version = "1.14";
+    static public final String version = "1.15";
     static public final String name    = "PixelFlow";
     static public final String author  = "Thomas Diewald";
     static public final String web     = "http://www.thomasdiewald.com";
